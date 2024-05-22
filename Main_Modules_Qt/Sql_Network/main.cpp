a6a8122bd724217f1918d37046329ac287a29e4a
“Ruzmetov” <f3lixxx@yandex.ru>
2024-05-22 14:42:23 +0300

    change HW2

--- Main_Modules_Qt/Sql_Network/main.cpp
+++ Main_Modules_Qt/Sql_Network/main.cpp
@@ -1,8 +1,25 @@
 #include <QCoreApplication>
+#include <QtSql/QSqlDatabase>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QDebug>
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(":memory:");
+
+    if(!db.open()){
+        qDebug() << "Failed to open database";
+    } else {
+        qDebug() << "Datatbase opened seccessfully";
+    }
+
+
+    QNetworkAccessManager manager;
+    qDebug() << "Network Access Manager created";
 
     return a.exec();
 }
